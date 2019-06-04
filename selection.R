f7b60daf8838b1372bb97bcbdaab2bd5bf181c47
pawpiotro <pawpiotro22@gmail.com>
2019-06-04 15:53:06 +0200

    GA fixed selection order

--- selection.R
+++ selection.R
@@ -1,8 +1,8 @@
 selection <- function(population, fitness, n){
-  tmp <- matrix(cbind(fitness, population), ncol = 2)
-  tmp <- tmp[order(tmp[,1], decreasing=TRUE),]
-  tmp <- tmp[1:n,]
-  pop <- tmp[,2]
-  fit <- tmp[,1]
-  list(pop, fit)
+  order <- order(fitness)
+  fitness <- fitness[order]
+  population[order,]
+  fitness <- fitness[1:n]
+  population <- population[1:n]
+  list(population, fitness)
 }
